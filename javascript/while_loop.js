2756a55184e451831ab536d54235b635e55ce7c0
Manishyadav69 <136569009+Manishyadav69@users.noreply.github.com>
2024-09-30 10:54:32 +0530

    Add files via upload

---
+++ javascript/while_loop.js
@@ -0,0 +1,42 @@
+//while Loop syntax
+// while (condition) {
+    
+// }
+
+// let index=0
+// while ( index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index =index + 2
+// }
+
+// let myArray=["manish","harish","arti"] 
+// let arr=0
+
+// while(arr<myArray.length){
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr+=1
+
+// }
+
+// let score=0
+// while (score <=50) {
+
+//     console.log(`Score is ${score}`);
+//     score+=2
+// }
+
+
+
+// let score=1
+
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+    
+// } while (score <= 10);
+
+// let score = 11
+// do {
+//     console.log(`score is ${score}`);
+    
+// } while (score<10);
